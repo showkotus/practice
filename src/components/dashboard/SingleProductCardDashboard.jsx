8807676d8b58a4bed1ef2284789200e1354f4428
Showkot <showkotus@gmail.com>
2024-05-26 09:56:46 +0600

    1st

---
+++ src/components/dashboard/SingleProductCardDashboard.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+
+// eslint-disable-next-line react/prop-types
+const SingleProductCardDashboard = ({ shoe, onDelete }) => {
+  const { id, title, brand, price, description, image_url } = shoe;
+
+  const handleDelete = async () => {
+    await fetch(`http://localhost:3000/shoes/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        onDelete(id);
+      });
+  };
+
+  return (
+    <div className="card w-96  bg-base-100 shadow-xl items-center justify-center">
+      <figure>
+        <img src={image_url} alt="Shoes" />
+      </figure>
+      <div className="card-body justify-center items-center">
+        <h2 className="card-title w-full rounded-md bg-yellow-400 justify-center ">
+          {title}
+        </h2>
+        <h3 className="text-xl font-semibold">{brand}</h3>
+        <h3 className="text-xl font-semibold">{price}</h3>
+        <p className=" text-justify">{description}</p>
+        <div className="card-actions justify-between">
+          <button className="btn bg-indigo-500 text-white">
+            <Link to={`/products/${id}`}>See details</Link>
+          </button>
+          <button className="btn bg-green-600 text-white">
+            <Link to={`edit/${id}`}>Edit</Link>
+          </button>
+          <button onClick={handleDelete} className="btn bg-red-500 text-white">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleProductCardDashboard;
